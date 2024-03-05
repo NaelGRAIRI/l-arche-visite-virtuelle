cfa87b28d2a88392ee0d89245df5c95d56b0ab47
HooleTheOwl <90103315+NaelGRAIRI@users.noreply.github.com>
2024-03-05 10:21:53 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_1748",
+      "name": "IMG_1748",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.509916941880407,
+        "pitch": 0.10041140063457021,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.837823434556153,
+          "pitch": 0.048551913511566624,
+          "rotation": 0,
+          "target": "1-img_1747"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_1747",
+      "name": "IMG_1747",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5825997813050332,
+        "pitch": 0.2140766931602922,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7927592473812659,
+          "pitch": 0.578808682675195,
+          "rotation": 0,
+          "target": "0-img_1748"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
